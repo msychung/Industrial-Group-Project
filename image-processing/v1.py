b3c5ba86438eb736b2d5797fc686c639140b9ab2
Tom Fell <73170205+twf2360@users.noreply.github.com>
2020-12-10 12:01:52 +0000

    Update v1.py
    made some mistakes when adding the functions into a class, hopefully fixed now

--- image-processing/v1.py
+++ image-processing/v1.py
@@ -1,3 +1,5 @@
+
+
 import math
 import skimage
 from skimage import io, viewer, color, data, filters, feature, morphology, exposure
@@ -32,19 +34,19 @@ class linefinder:
         returns:
         The np array containing the blurred sample 
         '''
-        sample_blur = ndimage.gaussian_filter(original, sigma=sigma)
+        sample_blur = ndimage.gaussian_filter(self.original, self.sigma)
         
         if view_plot == True:
             fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(2.5,8))
             fig.suptitle('Effect of Guassian Blur')
 
-            ax[0][0].imshow(original, cmap='gray')
+            ax[0][0].imshow(self.original, cmap='gray')
             ax[0][0].set(xlabel='', ylabel='', title = 'Original Greyscale Sample')
 
             ax[0][1].imshow(sample_blur, cmap='gray')
-            ax[0][1].set(xlabel='', ylabel='', title = 'Blurred Version, sigma ={}'.format(sigma))
+            ax[0][1].set(xlabel='', ylabel='', title = 'Blurred Version, sigma ={}'.format(self.sigma))
 
-            ax[1][0].plot(np.arange(0,np.size(original[200]) ,1), original[200])
+            ax[1][0].plot(np.arange(0,np.size(self.original[200]) ,1), self.original[200])
             ax[1][0].set(xlabel='Pixel Number', ylabel='Pixel Value', title='Values along 200th row \n for the original sample')
 
 
@@ -67,19 +69,19 @@ class linefinder:
         returns:
         returns the positions of the maximum positions according the gaussian blur method 
         '''
-        x = blur_sample_gauss(original, sigma, False)
+        x = linefinder.blur_sample_gauss(self,view_plot = False)
         max_positions = argrelextrema(x[200],np.greater)
         
         if view_plot == True:
             fig, ax = plt.subplots(ncols=3, nrows=1)
             fig.suptitle('Detecting lines though Guassian Blur')
             ax[0].imshow(x, cmap='gray')
-            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(sigma))
+            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(self.sigma))
 
             ax[1].plot(np.arange(0,np.size(x[200]), 1), x[200])
             ax[1].set(xlabel='', ylabel='', title = 'Values along 200th row \n for the blurred sample')
 
-            ax[2].imshow(original, cmap='gray')
+            ax[2].imshow(self.original, cmap='gray')
             ax[2].vlines(max_positions, color = 'red', ymin=0, ymax=500, linewidth=1)
             ax[2].set(xlabel='', ylabel='', title='Detected lines')
 
@@ -99,7 +101,7 @@ class linefinder:
         returns:
         array of the fourier transform of the 200th row of the blurred sample 
         '''
-        x = blur_sample_gauss(original, sigma, view_plot=False)
+        x = linefinder.blur_sample_gauss(self, view_plot=False)
         sample_FT = fftpack.fft(x[200])
         
         FTabs = np.abs(sample_FT)
@@ -108,7 +110,7 @@ class linefinder:
             fig, ax = plt.subplots(ncols=3, nrows=1)
             fig.suptitle('Fourier Transform after Blurring')
             ax[0].imshow(x, cmap='gray')
-            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(sigma))
+            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(self.sigma))
 
             ax[1].plot(np.arange(0,np.size(x[200]), 1), x[200])
             ax[1].set(xlabel='', ylabel='', title = 'Values along 200th row \n for the blurred sample')
@@ -123,26 +125,32 @@ class linefinder:
     def find_lines_fourier(self, view_plot=True):
         '''
         '''
-        x = blur_sample_gauss(original,sigma, False)
-        y = FT_blur(original,sigma, False)
+        x = linefinder.blur_sample_gauss(self,False)
+        y = linefinder.FT_blur(self,False)
         max_positions = argrelextrema(np.real(y), np.greater)
         
         if view_plot == True:
             fig, ax = plt.subplots(ncols=3, nrows=1)
             fig.suptitle('Detecting lines though Fourier Transform')
             ax[0].imshow(x, cmap='gray')
-            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(sigma))
+            ax[0].set(xlabel='', ylabel='', title = 'Blurred Sample, sigma = {}'.format(self.sigma))
 
             ax[1].plot(np.arange(0,np.size(x[200]), 1), x[200])
             ax[1].set(xlabel='', ylabel='', title = 'Values along 200th row \n for the blurred sample')
 
-            ax[2].imshow(original, cmap='gray')
+            ax[2].imshow(self.original, cmap='gray')
             ax[2].vlines(max_positions, color = 'red', ymin=0, ymax=500, linewidth=1)
             ax[2].set(xlabel='', ylabel='', title='Detected lines')
 
             plt.show()
         
         return max_positions
+
+
+
+
+
+
 
 
 
