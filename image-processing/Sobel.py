71cbf13f13c54531641e69691dcbe253d1e9b0f9
Michael Revell <michaeljrevell@gmail.com>
2020-12-16 18:54:11 +0000

    Finish code for Sobel edge detection

--- image-processing/Sobel.py
+++ image-processing/Sobel.py
@@ -1,7 +1,6 @@
-#%matplotlib inline
-
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import color
 
 #define the vertical filter
 vertical_filter = [[-1,-2,-1], [0,0,0], [1,2,1]]
@@ -9,8 +8,9 @@ vertical_filter = [[-1,-2,-1], [0,0,0], [1,2,1]]
 #define the horizontal filter
 horizontal_filter = [[-1,0,1], [-2,0,2], [-1,0,1]]
 
-#read in the pinwheel image
-img = plt.imread('Sample 1; 600dpi.jpeg')
+#read in the image
+scan = plt.imread('Sample 1; 75dpi.jpeg')
+img = color.grey2rgb(scan)
 
 #get the dimensions of the image
 n,m,d = img.shape
@@ -22,7 +22,7 @@ edges_img = img.copy()
 for row in range(3, n-2):
     for col in range(3, m-2):
         
-        #create little local 3x3 box
+        #create 3x3 box
         local_pixels = img[row-1:row+2, col-1:col+2, 0]
         
         #apply the vertical filter
@@ -43,3 +43,6 @@ for row in range(3, n-2):
 
 #remap the values in the 0-1 range in case they went out of bounds
 edges_img = edges_img/edges_img.max()
+
+plt.imshow(edges_img, cmap='gray')
+plt.show()
