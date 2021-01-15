6ae23b2cf8e0a2e20c4cf4acfbca26d99155274d
Michael Revell <michaeljrevell@gmail.com>
2021-01-15 19:22:38 +0000

    added sample scans

--- image-processing/UI.py
+++ image-processing/UI.py
@@ -17,15 +17,15 @@ scans = []
 scans_folder = Path("75dpi_scans") #relative path to the file with the scans. Quite simple for my current setup but may not always be the case
 np_array_scans = []
 
-new_files = input('enter new files, or find lines in previously saved files? Respond with either scan, or saved').lower()
+new_files = input('enter new files, or find lines in previously saved files? Respond with either scan, or saved: ').lower()
 
 if new_files == 'scan':
-    files_to_load = input('How many files would you like to scan in')
+    files_to_load = input('How many files would you like to scan in? ')
     int_files_to_load = int(files_to_load)
 
     i = 1
     while i <= int_files_to_load:
-        filename_input = input('please type the name of file number {}'.format(i))
+        filename_input = input('please type the name of file number {}: '.format(i))
         filename = str(filename_input)
         file = scans_folder / filename
         if not file.exists():
@@ -41,10 +41,10 @@ if new_files == 'scan':
         np.save('{}.npy'.format(scans[scan]), file, allow_pickle=True) #would be nice to change this so that things weren't saved as .jpeg.npy, however, it works! 
         np_array_scans.append(file)
 
-    print('Please respond Yes or no to the following questions')
-    test_now = input('Would you like to test your inputted scans for lines? If no, files are saved for later use  ').lower()
-    set_paramaters = input('Would you like to set your own paramters or use the presets? (yes for presets, no for custom paramaters)   ').lower()
-    view_plot = input('Would you like to view the output plot from the linefinder?').lower()
+    print('Please respond Yes or no to the following questions.')
+    test_now = input('Would you like to test your inputted scans for lines? If no, files are saved for later use ').lower()
+    set_paramaters = input('Would you like to set your own paramters or use the presets? (yes for presets, no for custom paramaters) ').lower()
+    view_plot = input('Would you like to view the output plot from the linefinder? ').lower()
 
     if test_now == 'yes':
         for scan in range(len(np_array_scans)):
@@ -61,11 +61,11 @@ if new_files == 'scan':
                     print('Result for Sample {}'.format(scans[scan]))
                     finder.severity(4,False)
             if set_paramaters == 'no':
-                blur_imp = input('Set sigma value for gaussian blur')
+                blur_imp = input('Set sigma value for gaussian blur: ')
                 blur = int(blur_imp)
-                row_imp = input('Set row number that is checked')
+                row_imp = input('Set row number that is checked: ')
                 row = int(row_imp)
-                baseline_imp = input('How severe do lines need to be to be recorded, out of 10 - Note: 8 out of 10 is the reccommended value')
+                baseline_imp = input('How severe do lines need to be to be recorded, out of 10? - Note: 8 out of 10 is the reccommended value ')
                 baseline = int(baseline_imp)/2
                 original = np_array_scans[scan]
                 finder = linefinder(original, blur, row)
@@ -85,7 +85,7 @@ if new_files == 'scan':
 elif new_files == 'saved':
 
     while True:
-        filename = input('Please enter the filename of the samples you would like to scan, or STOP when all names have been entered')
+        filename = input('Please enter the filename of the samples you would like to scan, or STOP when all names have been entered: ')
         if filename == 'STOP' or filename == 'Stop' or filename == 'stop':
             break
         else:
@@ -100,9 +100,9 @@ elif new_files == 'saved':
                 scans.append(file)
                 print('file added')
 
-    print('Please respond Yes or no to the following questions')
-    set_paramaters = input('Would you like to set your own paramters or use the presets? (yes for presets, no for custom paramaters)   ').lower()
-    view_plot = input('Would you like to view the output plot from the linefinder?').lower()
+    print('Please respond Yes or no to the following questions. ')
+    set_paramaters = input('Would you like to set your own paramters or use the presets? (yes for presets, no for custom paramaters) ').lower()
+    view_plot = input('Would you like to view the output plot from the linefinder? ').lower()
     for scan in range(len(scans)):
         if set_paramaters == 'yes':
                 original = scans[scan]
@@ -123,11 +123,11 @@ elif new_files == 'saved':
                 and the 'baseline'. if the mean prominence of the peaks is higher than the baseline, then the sample fails, so this baseline is essentially how severe is the test! 
                 '''
 
-                blur_imp = input('Set sigma value for gaussian blur')
+                blur_imp = input('Set sigma value for gaussian blur: ')
                 blur = int(blur_imp)
-                row_imp = input('Set row number that is checked')
+                row_imp = input('Set row number that is checked: ')
                 row = int(row_imp)
-                baseline_imp = input('How severe do lines need to be to fail the sample, out of 10 - Note: 8 out of 10 is the reccommended value') 
+                baseline_imp = input('How severe do lines need to be to fail the sample, out of 10? - Note: 8 out of 10 is the reccommended value ') 
                 baseline = int(baseline_imp)/2
                 original = scans[scan]
                 finder = linefinder(original, blur, row)
