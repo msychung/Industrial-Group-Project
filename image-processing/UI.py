f251c23e77d92104fc5558cb2206ea4a137afa5e
Melissa Chung <melissachung888@gmail.com>
2021-02-11 12:25:23 +0000

    Moved information() from UI to info.py,edited init

--- image-processing/UI.py
+++ image-processing/UI.py
@@ -8,6 +8,7 @@ import os
 import sys
 import warnings
 from linefinder import Linefinder
+from info import Info
 warnings.filterwarnings("ignore")
 
 class UserInterface():
@@ -15,70 +16,12 @@ class UserInterface():
     def __init__(self):
         # D:\Tom\Documents\Physics\PHYS355\phys355_code\scans_75dpi <- my own personal path
         print("*** Welcome to the User Interface for evaluating Machine Direction line severity in TFP nonwoven samples! *** \nPlease ensure sample scans are pre-arranged in folders, saved as .jpeg and are of the same material type and areal weight class. You will be led through a series of prompts for which responses must be entered, before results are delivered. Type CTRL+C into the terminal to quit the program at any point. \n") #Type 'back' if you wish to return to a previous question and change your answer.
-        self.information()
+        printInfo = Info()
+        printInfo.information()
         self.path = Path(self.get_path()) 
         self.material_types = {1: 'Carbon Veil', 2: 'Metal-Coated Carbon Veil'}  
         self.files = []
         
-
-    def information(self):
-        print('***Please respond yes or no to the following question***')
-        question = input('Would you like an additional explanation of how the code works, or what the results of this program will tell you? \nNote: There are many prompts, throughout the process that allow you to gain more information and insight to the code that are more relevant to specific processes. \nResponse: ').lower()
-        if not question in ('yes', 'no'):
-            self.yesno_error()
-            self.information()
-        if question == 'yes':
-            works_or_results = input('\nWould you like information on the working of the code, or on the results? \nPlease respond "workings", "results", or "both". \nResponse: ').lower()
-            if not works_or_results in ('workings', 'results', 'both'):
-                print('Response not recognised, please try again \n')
-                self.information()
-            if works_or_results == 'results':
-                print('The results of the code will be a number score, a score out of 10, and a pass or failed mark. All of these results are subject to change based on the parameters chosen.')
-                print('The number score will show the least variation. It will change if the same sample is blurred using a different sigma value for the guassian blur, but only that.')
-                print('The number score is the exact value of the mean peak prominence across the entire sample. This mean prominence represents the difference between light intensity maxima - the machine direction lines and the baseline value of the sample.')
-                print('The score out of ten is subject to whether or the sample is being compared to all samples of the same type, or just samples in a similar areal weight class.')
-                print('This gives a score, approximately between zero and ten for the severity of machine direction lines in the current sample, compared to similar samples.')
-                print('note: the best and worst samples that were sent to Lancaster University defined a score of ten, and zero respectively. Therefore testing samples that give results not between zero and ten are samples that lie outside of this range.')
-                print('Testing against all samples of the same material - or sample type - is recommended, as this gives a better appreciation for the visual severity of MD lines, which does not depend on areal weight.')
-                print('The pass fail mark depends on whether the number score is above or below the defined value, that yiu will be later prompted to enter.')
-                print('Our recommendations are included in this prompt, and you can also respond "info" for more information at that time.')
-                print('Plots of detected lines can also be viewed, however this was implemented more for the testing process. If a visual representation of machine direction lines is required, just looking at the sample is better!')
-                print('For more information, please contact the code authors through the github repository: https://github.com/msychung/Industrial-Group-Project \n \n')
-
-            if works_or_results == 'workings':
-                print('The linefinding process relies primarily on two modules: sci-kit image, and scipy signal.')
-                print('The linefinding process is also dependent on several parameters, as will be prompted to you as you go through the code.')
-                print('The key dependent of the linefinding process, is sample type - the material from which the sample is made. Only one type can be entered at a time.')
-                print('This is due to the fact that the different materials lead to very different results, and so they cannot be directly compared.')
-                print('Sci-kit image is used to read the sample in as a numpy array. It then undergoes a gaussian blur to minimise noise.')
-                print('Various programs within the scipy signal module are then used to find all of the peaks in all of the individual rows of the sample.')
-                print('Once all of the peaks in the sample have been found, the prominence of the peaks are calculated, to judge the difference between the peak and the baseline of the sample.')
-                print('The bigger this difference, the more visbile the corresponding machine direction line is. The mean of all of the peak prominences is then calculated.')
-                print('This mean value is used to determine the severity of machine direction lines in a sample.')
-                print('For more information, please contact the code authors, or view the code, through the github repository: https://github.com/msychung/Industrial-Group-Project \n \n')
-
-            if works_or_results == 'both':
-                print('The results of the code will be a number score, a score out of 10, and a pass or failed mark. All of these results are subject to change based on the parameters chosen')
-                print('The number score will show the least variation. It will change if the same sample is blurred using a different sigma value for the guassian blur, but only that')
-                print('The number score is the exact value of the mean peak prominence across the entire sample. This mean prominence represents the difference between light intensity maxima - the machine direction lines and the baseline value of the sample')
-                print('The score out of ten is subject to whether or the sample is being compared to all samples of the same type, or just samples in a similar areal weight class')
-                print('This gives a score, approximately between zero and ten for the severity of machine direction lines in the current sample, compared to similar samples')
-                print('note: the best and worst samples that were sent to Lancaster University defined a score of ten, and zero respectively. Therefore testing samples that give results not between zero and ten are samples that lie outside of this range')
-                print('Testing against all samples of the same material - or sample type - is recommended, as this gives a better appreciation for the visual severity of MD lines, which does not depend on areal weight')
-                print('The pass fail mark depends on whether the number score is above or below the defined value, that yiu will be later prompted to enter')
-                print('Our recommendations are included in this prompt, and you can also respond "info" for more information at that time')
-                print('Plots of detected lines can also be viewed, however this was implemented more for the testing process. If a visual representation of machine direction lines is required, just looking at the sample is better!')
-                
-                print('\nThe linefinding process relies primarily on two modules: sci-kit image, and scipy signal')
-                print('The linefinding process is also dependent on several parameters, as will be prompted to you as you go through the code.')
-                print('The key dependent of the linefinding process, is sample type - the material from which the sample is made. Only one type can be entered at a time.')
-                print('This is due to the fact that the different materials lead to very different results, and so they cannot be directly compared.')
-                print('Sci-kit image is used to read the sample in as a numpy array. It then undergoes a gaussian blur to minimise noise')
-                print('Various programs within the scipy signal module are then used to find all of the peaks in all of the individual rows of the sample')
-                print('Once all of the peaks in the sample have been found, the prominence of the peaks are calculated, to judge the difference between the peak and the baseline of the sample')
-                print('The bigger this difference, the more visbile the corresponding machine direction line is. The mean of all of the peak prominences is then calculated')
-                print('This mean value is used to determine the severity of machine direction lines in a sample')
-                print('For more information, please contact the code authors, or view the code, through the github repository: https://github.com/msychung/Industrial-Group-Project \n \n')
 
     def get_path(self):
         response = input("Please specify the path to the folder containing the sample scan files. Enter 'help' for further explanation: ")
